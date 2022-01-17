002c0ca291a28be2c7df46552370d6632b79383d
Francisco Castañeda <dragalichi@gmail.com>
2022-01-17 13:53:59 -0600

    Fix asmdef

--- Runtime/TowiGameManager.cs
+++ Runtime/TowiGameManager.cs
@@ -1,9 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace PixframeStudios.TowiPlugin
 {    
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
     public class TowiGameManager : MonoBehaviour
     {
         public static TowiGameManager instance;
